83542663e69796be7f634bacc0c43bc0487e6927
IvanArt11 <artemiev1990@mail.ru>
2023-03-10 22:06:55 +0300

    added a new file hw-3.js and new section to index.html

---
+++ scripts/hw-3.js
@@ -0,0 +1,45 @@
+//Задание 1
+let i=0;
+while (i<2) {
+    console.log('Привет!');
+    i++;
+}
+
+//Задание 2
+let a=1;
+while (a<=5) {
+    console.log(a);
+    a++;
+}
+
+//Задание 3
+let b=7;
+while (b<=22) {
+    console.log(b);
+    b++;
+}
+
+// //Задание 4
+let obj = {'Коля': '200', 'Вася': '300', 'Петя': '400'};
+
+for(key in obj){
+    console.log([key]+ ' - зарплата '+ obj[key]+' долларов');
+}
+
+// //Задание 5
+let n = 1000;
+let num=0;
+
+while (n>50) {
+    num++;
+    n/=2;
+};
+
+console.log(n);
+console.log(num);
+
+//Задание 6
+const firstFriday=3;
+for (let i = firstFriday; i <= 31; i=i+7) {
+    console.log(`Сегодня пятница, ${i}-е число. Необходимо подготовить отчет.`);
+}
